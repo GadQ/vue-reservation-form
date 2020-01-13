801d2acb51972d0bc19ae6afb4867c7af1b6dcc7
Lukasz Berwald <lberwald@kaliop.com>
2020-01-13 15:01:46 +0100

    Add form validation

--- src/components/FormReservation.vue
+++ src/components/FormReservation.vue
@@ -1,5 +1,5 @@
 <template>
-    <form action="#" class="reservation" method="post">
+    <form action="#" class="reservation" method="post" @submit.prevent="sendForm">
         <div class="reservation__header">
             <div class="reservation__price">
                 <span class="sr-only">Price:</span>{{ price }} {{ currency }}
@@ -10,40 +10,51 @@
             </div>
         </div>
 
-        <fieldset class="reservation__dates">
-            <legend class="reservation__dates-legend">Dates</legend>
-            <div class="reservation__dates-buttons">
-                <button class="reservation__dates-button" :class="{ 'is-active': isCheckInActive}" type="button" @click.stop="toggleDatepicker" ref="checkInButton">
-                    {{ checkInLabel }}
-                </button>
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 22" width="28"
-                     class="reservation__dates-separator">
-                    <path
-                        d="M32 11c0-.27-.11-.52-.3-.71l-9.9-10a1 1 0 00-1.4 0c-.4.4-.4 1.04 0 1.43L28.58 10H1A1 1 0 000 11a1 1 0 001 1h27.59l-8.2 8.28c-.39.4-.39 1.03 0 1.42a1 1 0 001.42 0l9.9-9.99c.18-.19.29-.45.29-.71z"/>
-                </svg>
-                <button class="reservation__dates-button" :class="{ 'is-active': isCheckOutActive}" type="button" @click.stop="toggleDatepicker">
-                    {{ checkOutLabel }}
-                </button>
-            </div>
-
-            <transition name="slide-down">
-                <div class="reservation__datepicker" :class="{ 'is-checkout-active': isCheckOutActive}" v-show="datepickerOpen" @click.stop>
-                    <DatePicker
-                        :date-start="checkIn"
-                        :date-end="checkOut"
-                        :available-from="dates.availableFrom"
-                        :available-to="dates.availableTo"
-                        :unavailableDates="dates.unavailableDates"
-                        @dateFromChanged="checkInChanged"
-                        @dateToChanged="checkOutChanged"
-                    />
+        <div class="reservation__field">
+            <fieldset class="reservation__dates">
+                <legend class="reservation__dates-legend">Dates</legend>
+                <div class="reservation__dates-buttons">
+                    <button class="reservation__dates-button" :class="{ 'is-active': isCheckInActive}" type="button" @click.stop="toggleDatepicker" ref="checkInButton">
+                        {{ checkInLabel }}
+                    </button>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 22" width="28"
+                         class="reservation__dates-separator">
+                        <path
+                            d="M32 11c0-.27-.11-.52-.3-.71l-9.9-10a1 1 0 00-1.4 0c-.4.4-.4 1.04 0 1.43L28.58 10H1A1 1 0 000 11a1 1 0 001 1h27.59l-8.2 8.28c-.39.4-.39 1.03 0 1.42a1 1 0 001.42 0l9.9-9.99c.18-.19.29-.45.29-.71z"/>
+                    </svg>
+                    <button class="reservation__dates-button" :class="{ 'is-active': isCheckOutActive}" type="button" @click.stop="toggleDatepicker">
+                        {{ checkOutLabel }}
+                    </button>
                 </div>
+
+                <transition name="slide-down">
+                    <div class="reservation__datepicker" :class="{ 'is-checkout-active': isCheckOutActive}" v-show="datepickerOpen" @click.stop>
+                        <DatePicker
+                            :date-start="checkIn"
+                            :date-end="checkOut"
+                            :available-from="dates.availableFrom"
+                            :available-to="dates.availableTo"
+                            :unavailableDates="dates.unavailableDates"
+                            @dateFromChanged="checkInChanged"
+                            @dateToChanged="checkOutChanged"
+                        />
+                    </div>
+                </transition>
+            </fieldset>
+
+            <transition name="fade">
+                <div class="reservation__field-error" v-if="errors.dates">{{ errors.dates }}</div>
             </transition>
-        </fieldset>
+        </div>
+
 
         <div class="reservation__field">
             <label class="reservation__field-label">Full name</label>
             <input class="reservation__field-input" type="text" v-model="fullName">
+
+            <transition name="fade">
+                <div class="reservation__field-error" v-if="errors.fullName">{{ errors.fullName }}</div>
+            </transition>
         </div>
 
         <div class="reservation__footer">
@@ -64,7 +75,8 @@
             checkIn: null,
             checkOut: null,
             datepickerOpen: false,
-            fullName: null
+            fullName: '',
+            errors: {}
         }),
         props: {
             price: Number,
@@ -129,6 +141,58 @@
                 if( event.key === 'Escape' ) {
                     this.closeDatepicker();
                 }
+            },
+            validateName() {
+                if( this.fullName.trim() === '' ) {
+                    this.errors.fullName = 'Name cannot be empty';
+                }
+            },
+            validateDates() {
+                const isCheckInDate = this.checkIn instanceof Date;
+                const isCheckOutDate = this.checkOut instanceof Date;
+
+                if( !isCheckInDate && !isCheckOutDate ) {
+                    this.errors.dates = 'You have to choose dates for reservation';
+                    return;
+                }
+
+                if( !isCheckInDate ) {
+                    this.errors.dates = 'You have to choose check in date for reservation';
+                    return;
+                }
+
+                if( !isCheckOutDate ) {
+                    this.errors.dates = 'You have to choose check out date for reservation';
+                    return;
+                }
+
+                if( this.checkIn.getTime() > this.checkOut.getTime() ) {
+                    this.errors.dates = 'Check out date should be later than check in date';
+                    return;
+                }
+            },
+            isFormValid() {
+                this.validateName();
+                this.validateDates();
+
+                return Object.keys(this.errors).length === 0;
+            },
+            sendForm() {
+                this.clearErrors();
+
+                if( this.isFormValid() ) {
+                    // Send data to API/Store
+                    console.log('Data has been sent');
+                    this.resetForm();
+                }
+            },
+            clearErrors() {
+                this.errors = {};
+            },
+            resetForm() {
+                this.checkIn = null;
+                this.checkOut = null;
+                this.fullName = '';
             }
         },
         watch: {
@@ -154,6 +218,19 @@
 <style lang="scss">
     $color-gray: #ccc;
     $color-green: #00dbb1;
+
+    .fade-enter-active {
+        transition: opacity 200ms linear;
+    }
+
+    .fade-leave-active {
+        transition: opacity 100ms linear;
+    }
+
+    .fade-enter,
+    .fade-leave-to {
+        opacity: 0;
+    }
 
     .slide-down-enter,
     .slide-down-leave-to {
@@ -185,7 +262,7 @@
         &__dates {
             position: relative;
             border: 1px solid $color-gray;
-            margin: 45px 0 20px 0;
+            margin: 45px 0 0 0;
             padding: 8px;
             display: flex;
         }
@@ -287,6 +364,12 @@
                 border-color: $color-green;
             }
         }
+        
+        &__field-error {
+            font-size: 0.8em;
+            color: #e00;
+            margin-top: 2px;
+        }
 
         &__footer {
             margin-top: 15px;
@@ -295,7 +378,7 @@
         }
         
         &__submit {
-            padding: 16px 20px 15px 20px;
+            padding: 14px 20px 13px 20px;
             border: none;
             background: #da5f5e;
             font-weight: 600;
